4cf25036ce9127810f5e54601259340d5b748077
markusranda <randamarkus.95@gmail.com>
2022-05-14 15:41:08 +0200

    - Added new font for title - Imported app.css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <script>
+	import '../assets/app.css';
+
 	import Boid from '$lib/boid/boid';
 	import Victor from 'victor';
 	import Surfer from '$lib/Surfer.svelte';
@@ -17,12 +19,14 @@
 		mouse.y = event.clientY;
 	}
 
-	var counter = 0
+	var counter = 0;
 	let /** @type {Boid[]} */ boids = [...Array(30)].map(() => new Boid(counter++));
 </script>
 
-<div on:mousemove={handleMouseMove}>
-	<div>DET ER SOMMER I HELE VERDEN</div>
+<div id="app-container" on:mousemove={handleMouseMove}>
+	<div>
+		<h1 class="main-text">DET ER SOMMER I HELE VERDEN</h1>
+	</div>
 
 	{#each boids as boid, i}
 		<Surfer {mouse} {boid} {boids} startPos={new Victor(i * 40, i)} />
@@ -30,8 +34,33 @@
 </div>
 
 <style>
-	div {
-		width: 100%;
-		min-height: 100vh;
+	h1 {
+		margin: 0;
+	}
+
+	#app-container {
+		padding: 1rem;
+		height: 100%;
+	}
+
+	.main-text {
+		font-size: 15rem;
+		background-image: linear-gradient(
+			to right,
+			#2f61fe,
+			#0075fb,
+			#0083f0,
+			#008ee1,
+			#0096cf,
+			#28a0cd,
+			#44a9ca,
+			#5db2c7,
+			#74c2d4,
+			#8ad3e1,
+			#9fe3ef,
+			#b5f4fd
+		);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
 	}
 </style>
